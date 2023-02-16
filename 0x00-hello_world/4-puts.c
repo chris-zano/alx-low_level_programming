4e2bc7f970a5d8d715b452a3f1b51103809c9eb1
Christian Solomon <cncs.chris@gmail.com>
2023-02-16 22:41:33 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ *main - a program that prints text with the puts function
+ * */
 
-int main(void) {
-	puts("Programming is like building a multilingual puzzle\n");
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
