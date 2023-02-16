8301be6bccb4fa8b933faee9a814fefb7df7bd99
Christian Solomon <cncs.chris@gmail.com>
2023-02-16 23:37:15 +0000

    correction of 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - program that prints some text
- * Return : 1 (Success)
+ * Return: 1 (Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 	return (1);
 }
