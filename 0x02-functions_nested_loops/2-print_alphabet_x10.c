277aa9614dfaa8abe23182d916edf4ac1a21dad5
Christian Solomon <cncs.chris@gmail.com>
2023-02-21 15:47:10 +0000

    correction of 2-print_alphabets_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10.c - prints the alphabet ten times
+ * Return: void 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
