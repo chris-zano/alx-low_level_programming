0ddab50ca987525a605f32764522d3897685b1ad
Christian Solomon <cncs.chris@gmail.com>
2023-02-23 19:30:52 +0000

    largest factorial

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,8 +7,22 @@
 
 int main(void)
 {
-	long int b = 612852475143;	
-	
-	printf("%ld\n", b);
+	long int number = 612852475143;
+	long int large_factor = 0;
+	long int i = 2;
+
+	while (number != 1)
+	{
+		if (number % i == 0)
+		{
+			while (number % i == 0)
+			{
+				number /= i;
+			}
+			large_factor = i;
+		}
+		i++;
+	}
+	printf("%ld\n", large_factor);
 	return (0);
 }
