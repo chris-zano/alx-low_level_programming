9a5a823404908b5fb89495008474fdfc7b951ec6
Christian Solomon <cncs.chris@gmail.com>
2023-03-06 03:58:29 +0000

    Submission of 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strchr - locates a character in a string and returns pointer it
+ * @s: pointer to a string
+ * @c: character to search for
+ * Return: pointer to c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+}
