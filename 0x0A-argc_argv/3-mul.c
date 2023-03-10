262df3e530825c5b9e6d5bc4fb11a584984aec2d
Christian Solomon <cncs.chris@gmail.com>
2023-03-10 17:06:48 +0000

    submission of 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that multiples two arguments
+ * @argc: argument counter - returns number of args in cl
+ * @argv: argument vector - returns pointer to array of strings
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc - 1 >= 1 && argc - 1 <= 2)
+	{
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
+
+		result = a * b;
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	return (0);
+}
