cae9a456339efe9fac4bfed8adebf9c4f2f36007
Christian Solomon <cncs.chris@gmail.com>
2023-03-16 14:35:10 +0000

    correction of 1

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	    char *concat;
 
-	        concat = string_nconcat("Best ", "School !!!", 13);
+	        concat = string_nconcat(NULL, "HolbertonSchool", 10);
 		    printf("%s\n", concat);
 		        free(concat);
 			    return (0);
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -48,11 +48,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *conStr;
 
 	i = 0;
-	s2 = find_s2(s2, i, n);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	s2 = find_s2(s2, i, n);
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
