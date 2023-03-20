cb2cb1045f6148db98b139956a51ba3726ce4833
Christian Solomon <cncs.chris@gmail.com>
2023-03-20 20:19:03 +0000

    submission of dog.h - repo init

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *  struct dog - constructors of a dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: structure for info about dogs
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
