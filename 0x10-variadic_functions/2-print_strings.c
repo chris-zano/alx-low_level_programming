fbc47be81a5662ca407c62f875039e5fe95f90cb
Christian Solomon <cncs.chris@gmail.com>
2023-03-23 19:50:57 +0000

    submission of 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, char *));
+		if (va_arg(ap, char *) == NULL)
+		{
+			printf("%s", va_arg(ap, char *));
+		}
+		else
+		{
+			printf("%s", va_arg(ap, char *));
+		}
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
