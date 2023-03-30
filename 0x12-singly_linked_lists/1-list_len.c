001ed555a995d46e9c4b85eb32fee4c36cfec105
Christian Solomon <cncs.chris@gmail.com>
2023-03-30 22:39:57 +0000

    correction -> advanced

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_list - prints all the elements of a list_t list
+ * list_len - returns the number of elements in a linked list_t list
  * @h: head of list
  * Return: number of nodes
  */
