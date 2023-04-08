3a66a2f93e18ad492e22f243ebc605ffc1e7a750
Christian Solomon <cncs.chris@gmail.com>
2023-04-08 10:30:06 +0000

    submission -> advanced[100]

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * get_endianness - checks endianness
+ * Return: 0 if big endian, else 1
+ */
+
+int get_endianness(void)
+{
+	/* set n at struct endian to 1 */
+	endian.n = 1;
+
+	if (endian.c[0] == 1)
+		/*if the first bit is 1, then its a LE*/
+		return (1);
+	return (0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * union - a union data type with two members
+ * @n: integer number
+ * @c: an array of size sizeof(n)
+ * Description - a union that holds an unteger and an array
+ */
+union
+{
+	int n;
+	char c[sizeof(int)];
+} endian;
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -8,6 +21,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
+int get_endianness(void);
 
 #endif /* MAIN_H */
