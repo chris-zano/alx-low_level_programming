4672cffd88c4f31c322dd74436e6523ba280685f
Christian Solomon <cncs.chris@gmail.com>
2023-04-08 09:50:11 +0000

    correction -> task[5] try 1

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,15 +12,20 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int counter;
 	unsigned long int bit_length, index;
 
+	/* XOR m with x to flip the bits so they match */
 	n_xor_m = n ^ m;
-	bit_length = (sizeof(n_xor_m) * 8);
+	/* find the number of bits in n_xor_m */
+	bit_length = (sizeof(n_xor_m) * 8); /* x8 - 8 bits in a byte */
 	counter = 0;
 
-	for (index = 0; index < bit_length; index ++)
+	/* at each bit position */
+	for (index = 0; index < bit_length; index++)
 	{
+		/* count number of bits to be flipped using >> and & */
 		counter = counter + (n_xor_m & 1);
+		/* right sift n_xor_m by 1 bit */
 		n_xor_m >>= 1;
 	}
-
+	/* return the count */
 	return (counter);
 }
