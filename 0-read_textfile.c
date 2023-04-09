0cda954ed67191344bda16308a815b552c4873f8
Christian Solomon <cncs.chris@gmail.com>
2023-04-09 00:27:46 +0000

    submission -> task[0]

--- 0-read_textfile.c
+++ 0-read_textfile.c
@@ -1,9 +1,9 @@
 #include "main.h"
-
 #include <unistd.h>
 #include <fcntl.h>
-
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - reads a file and prints it to POSIX standard output
@@ -16,5 +16,36 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	return (0);
+	ssize_t fd;
+	ssize_t read_f;
+	ssize_t byte_w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(buffer) * letters);
+
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY) ;
+	read_f = read(fd, buffer, letters);
+	byte_w = write(STDOUT_FILENO, buffer, read_f);
+
+	if (fd == -1 || read_f == -1 || byte_w == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	if (byte_w != read_f)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (byte_w);
 }
