8d15c9229b18c7799114fcab20b64a618376f240
Christian Solomon <cncs.chris@gmail.com>
2023-04-09 21:31:43 +0000

    correction -> task[2] try 5

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,11 +25,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		while (text_content[index] != '\0')
-		{
+		for (length = 0; text_content[length];)
 			length++;
-			index++;
-		}
 	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
