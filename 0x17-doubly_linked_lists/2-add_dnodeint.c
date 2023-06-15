adc627ac143f566656df9d9008ca5a01e9a2ea4c
Christian Solomon <cncs.chris@gmail.com>
2023-06-15 14:35:33 +0000

    submission of 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,26 +8,19 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *temp = *head;
 	dlistint_t *new;
 
-	if (temp == NULL)
+	if (head == NULL)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (temp)
-	{
-		new->next = temp;
-		new->prev = temp->prev;
-		temp->prev = new;
-		temp = new;
-		return (temp);
-	}
-	new->next = temp;
 	new->prev = NULL;
-	temp = new;
-	return (temp);
+	new->next = *head;
+	*head = new;
+	if (new->next != NULL)
+		(new->next)->prev = new;
+	return (new);
 }
