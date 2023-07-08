c8ad22c9e96b92dc515d2066dc17c03cc46e4de0
Christian Solomon <cncs.chris@gmail.com>
2023-07-08 02:22:16 +0000

    submission of tasks

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *current, *new_node;
+	const unsigned char *str = (const unsigned char *)key;
+
+	if (!ht || !key || *key == '\0')
+		return (0);
+
+	index = hash_djb2(str) % ht->size;
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+
+	ht->array[index] = new_node;
+	return (1);
+}
