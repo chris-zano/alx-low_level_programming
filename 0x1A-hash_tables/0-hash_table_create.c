00e9887862948b12b32ce2a9d531f5c4f7bdc61f
Christian Solomon <cncs.chris@gmail.com>
2023-07-08 01:57:12 +0000

    submission of tasks

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,23 +7,24 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	unsigned long int i;
+	hash_table_t *table = NULL;
+	hash_node_t **arr = NULL;
+	unsigned long int i = 0;
+
 
 	table =  malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 
-	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * size);
-	if (!table->array)
-	{
-		free(table);
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (!arr)
 		return (NULL);
-	}
 
-	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
+	for (; i < size; ++i)
+		arr[i] = NULL;
+
+	table->size = size;
+	table->array = arr;
 
 	return (table);
 }
